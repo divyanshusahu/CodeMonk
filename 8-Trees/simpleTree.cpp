fabf4fb818fd6f3fe6343f3c8de819b8285b8f5f
Divyanshu Sahu <dsahu1997@gmail.com>
2019-07-27 12:02:31 +0530

    Trees problems added

---
+++ 8-Trees/simpleTree.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+};
+
+struct node* newNode(int data)
+{
+    struct node* temp = (struct node*)malloc(sizeof(struct node));
+    temp->data = data;
+    temp->left = NULL;
+    temp->right = NULL;
+    return temp;
+}
+
+int main()
+{
+    struct node *root = newNode(1);
+    root->left = newNode(2);
+    root->right = newNode(3);
+    root->left->left = newNode(4);
+
+    return 0;
+}
